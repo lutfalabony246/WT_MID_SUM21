b4b07ea033bf9babd74732b484b248f16e4ba6a5
USER <18-36187-1@student.aiub.edu>
2021-06-23 10:02:10 +0600

    2 form

---
+++ lab6MidProject/ProductInfo.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Product Info</title>
+</head>
+<body>
+	<fieldset style="width:270px">
+		<table>
+			<tr>
+			<td>Product Id*</td>
+			<td>:<input type="text" name=""></td>
+		</tr>
+		<tr>
+			<td>Product Name*</td>
+			<td>:<input type="text" name=""></td>
+		</tr>
+		<tr>
+			<td>Price*</td>
+			<td>:<input type="text" name=""></td>
+		</tr>
+		<td>Quantity*</td>
+  <td>:<input type="number" id="quantity" name="quantity" min="1" max="5"></td>
+  <tr>
+		
+			<td>Date*</td>
+			<td>:<input type="date" name="begin" 
+        placeholder="dd-mm-yyyy" value=""
+        min="1997-01-01" max="2030-12-31"></td>
+		</tr></td>
+		</tr>
+		<tr>
+			<td>Category Id:</td>
+			<td><select>
+				<option>1</option>
+				<option>2</option>
+				<option>3</option>
+				<option>4</option>
+			</select></td>
+		</tr>
+		<tr>
+			<td></td>
+			<td><input type="submit" name=""></td>
+		</tr>
+		</table>
+	</fieldset>
+	
+</body>
+</html>
---
+++ lab6MidProject/supplierInfo.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+</head>
+<body>
+	<fieldset style="width:270px">
+		<table>
+			<tr>
+			<td>Supplier Id*</td>
+			<td>:<input type="text" name=""></td>
+		</tr>
+		<tr>
+			<td>Supplier Name*</td>
+			<td>:<input type="text" name=""></td>
+		</tr>
+		<tr>
+			<td>Supplier Username*</td>
+			<td>:<input type="text" name=""></td>
+		</tr>
+		<tr>
+			<td>Supplier MobileNumber*</td>
+			<td>:<input type="text" name="" placeholder="enter 11 digit mobile number"></td>
+		</tr>
+		<tr>
+			<td>Supplier email*</td>
+			<td>:<input type="text" name=""></td>
+		</tr>
+		<tr>
+			<td>Password*</td>
+			<td>:<input type="password" name=""></td>
+		</tr>
+		<tr>
+			<td>Confirm password*</td>
+			<td>:<input type="password" name=""></td>
+		</tr>
+		<tr>
+			<td></td>
+			<td>&nbsp;<input type="submit" name=""></td>
+		</tr>
+		</table>
+	</fieldset>
+	
+</body>
+</html>
